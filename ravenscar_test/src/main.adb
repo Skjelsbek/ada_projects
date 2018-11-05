530e7bc3464c48da54950beec13fb235faf2088f
Jon Skjelsbæk <skjelsbek@hotmail.com>
2018-11-05 11:33:10 +0100

    Using the ravenscar profile in combination with Ada.Real_Time doesn't work at all

--- ravenscar_test/src/main.adb
+++ ravenscar_test/src/main.adb
@@ -1,11 +1,11 @@
-with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Real_Time; --use Ada.Real_Time;
 with MicroBit.IOs; use MicroBit;
 procedure Main is
-   release_time: Time;
+   --release_time: Time;
 begin
-   release_time := Clock + Microseconds(10);
+   --release_time := Clock + Microseconds(10);
    IOs.set(0, True);
-   delay until release_time;
+   --delay until release_time;
    IOs.set(0, False);
    null;
 end Main;
